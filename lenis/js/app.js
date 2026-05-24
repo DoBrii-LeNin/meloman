b0677091fa381150ec9638e5d7781168fd7dd06c
manea Stanislav <stanislavmanea@gmail.com>
2026-05-24 21:37:37 +0300

    first.comit

---
+++ lenis/js/app.js
@@ -0,0 +1,8 @@
+var index = 15;
+var strok = "17";
+const color = "background";
+
+
+console.log("index = " + index + "." );
+console.log("stroka = " + strok + "." );
+console.log("background = " + color + "." );
