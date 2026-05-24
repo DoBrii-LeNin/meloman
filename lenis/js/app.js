ac4ff570dd086ca701af64c2a57459040af697df
manea Stanislav <stanislavmanea@gmail.com>
2026-05-25 00:33:59 +0300

    am realiza 2 teme

--- lenis/js/app.js
+++ lenis/js/app.js
@@ -1,8 +1,193 @@
-var index = 15;
-var strok = "17";
-const color = "background";
+// let num1 = 15;
+// let num2 = 13;
+// let suma = num1 + num2;
+
+// const color = "background";
+// const manual = "#353535"
+
+// const volume = 2 * 2 * 2;
+// const volume1 = 2 ** 6;
+
+// let age = 18 + 3;
+// console.log( age );
+// age += 3;
+// age -= 3;
+// age *= 3;
+// age /= 3;
+// age++;
+// age--;
 
 
-console.log("index = " + index + "." );
-console.log("stroka = " + strok + "." );
-console.log("background = " + color + "." );
+// const vasea = 20;
+
+// console.log( age > vasea );
+// console.log( age >= vasea );
+// console.log( age < vasea );
+// console.log( age <= vasea );
+// console.log( age == vasea );
+
+
+
+// console.log("index = " + suma );
+// console.log("background = " + color + "." + " " + volume + " " + volume1);
+// console.log( volume);
+// console.log( volume1);
+
+
+
+
+// const corobca = 100 - 10 > 90 - 5;
+// console.log(corobca)
+
+// let a = (6 + 10) / 2;
+// console.log(a);
+
+// a = "meloman";
+
+// console.log(a);
+
+
+// let c;
+// let v;
+// c = v = 100 + 50;
+// console.log(c);
+// console.log(v);
+
+ 
+// let a = 5;
+// let b = 6.3;
+// console.log(typeof a);
+// console.log(typeof b);
+// a = "linie";
+// console.log(typeof a);
+// let admin = false;
+// console.log(typeof admin);
+
+// let c; //undefined
+// console.log(typeof c);
+// c = 10;
+// console.log(typeof c);
+
+// let d = null;
+// console.log(typeof d );
+// console.log(d == null);
+// console.log(typeof (d == null));
+
+// const payRateUSD = 80;
+// const housr = 5;
+// const day = 5;
+
+// const projectHours = 40;
+
+// const deadLine = 11;
+
+// let weekly = housr * day;
+// let deadLineHours = (deadLine - 2) * housr;
+
+// const number = deadLineHours >= projectHours;
+
+// const pay = projectHours * payRateUSD;
+
+// console.log(number);
+// console.log(pay);
+
+// const projectName = "site magazin";
+// const price = 2000;
+// const author = "manea stanislav";
+
+// const template = author + " a cerut " + projectName + " cu pretul " + price + " lei";
+// console.log(template);
+
+// const template2 = `${author} a cerut ${projectName} cu pretul ${price} lei`;
+// console.log(template2);
+
+// const template3 = `proiectul \n` + `pretul: ` + price + ` lei`;
+// console.log(template3);
+
+// const template4 = `proiect
+// pretul: ${price} lei`;
+// console.log(template4);
+
+// const age = "18";
+// console.log(Number(age) + 5);
+// console.log(age - 3);
+// const userName = "Vasea";
+// console.log(Number(userName) + 5);
+// console.log(typeof NaN);
+// console.log(String(5) + 7);
+// console.log(Boolean(123));
+
+// const money = 100;
+
+// if (money > 50) {
+//     console.log("pate cumpara");
+// } else if ( money >5 ){
+//     console.log("cumpara mini productul");
+// } else {
+//     console.log("nu poate");
+// };
+
+// const deposit = 10000;
+// const rate = 0.07;
+// const depositLength = 24;
+// const houseCost = 13500;
+
+// const res = deposit * (1 + rate / 12) ** 24;
+
+
+// if (res > houseCost) {
+//     console.log(`noi am avut ${res} poate cumpara  restul ${res - houseCost}`);
+// } else {
+//     console.log("nu putem")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
